a4d980c57b21bcd019b5aa65fb843a457c7c9d76
Sang Dang <sangdth@gmail.com>
2019-05-29 09:49:31 +0300

    :tada:

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,82 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <div class="boxes wrapper">
+            <box-message
+                v-for="(obj, index) in options"
+                :key="index"
+                :title="obj.title"
+                :message="obj.message"
+                class="box item"
+            />
+        </div>
+
+        <div class="buttons wrapper">
+            <button
+                v-for="(obj, index) in options"
+                :key="index"
+                class="button item"
+                @click="modify(index)"
+            >
+                Select {{ obj.title }}
+            </button>
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BoxMessage from './components/BoxMessage.vue';
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+    name: 'app',
+    components: { BoxMessage },
+
+    data() {
+        return {
+            options: [
+                {
+                    title: 'Apple',
+                    message: 'Use dongle different',
+                },
+                {
+                    title: 'Microsoft',
+                    message: 'Your machine. Our update.',
+                },
+                {
+                    title: 'Default Title',
+                    message: 'Default Message',
+                },
+            ] 
+        };
+    },
+
+    methods: {
+        modify(index) {
+            this.options[index].message += ' modified'
+            if (index === 2) {
+                this.options[index].message = 'Default Message';
+            }
+        },
+    },
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.boxes, .wrapper {
+    display: flex;
+    justify-content: space-between;
+    width: 800px;
+    margin: 20px auto;
+}
+
+.box, .item {
+    width: 200px;
+}
+
+.button {
+    height: 100px;
+    width: 220px;
+    border: 4px solid #0F0;
+    color: #0F0;
+    background: #333;
 }
 </style>
---
+++ src/components/BoxMessage.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="single-box">
+        <h2>{{ title }}</h2>
+        <p>{{ message }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'BoxMessage',
+
+    props: {
+        title: {
+            type: String,
+            default: 'Default Title',
+        },
+        message: {
+            type: String,
+            default: 'Default Message',
+        },
+    },
+};
+</script>
+
+<style>
+.single-box {
+    border: 1px solid #333333;
+    padding: 10px;
+}
+</style>
